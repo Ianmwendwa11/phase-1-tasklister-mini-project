5cfebb3d059c65b169d70ff6187f7c956ff13c62
ianmwendwa11 <ian.musyoki12@gmail.com>
2025-03-24 11:51:55 +0300

    assign

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+ 
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  
+  form.addEventListener("submit", (event) => {
+    event.preventDefault(); 
+
+    const taskInput = document.getElementById("new-task-description")
+    const taskText = taskInput.value.trim();
+
+    if (taskText === "") {
+      alert("Please fill out this field");
+      return;
+    }
+
+    const listItem = document.createElement("li");
+    listItem.textContent = taskText;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = ("delete");
+    deleteButton.style.marginLeft = "10px";
+    deleteButton.addEventListener("click", () => {
+      listItem.remove();
+    });
+
+    listItem.appendChild(deleteButton);
+
+    taskList.appendChild(listItem);
+
+    taskInput.value = "";
+
+ });
 });
